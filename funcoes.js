5a7fec6ca94e0a5b35d4c3af6c2b625de2a6141a
RafaelSouza2007 <rafael13play@gmail.com>
2024-10-08 23:05:17 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,26 @@
+/*Código para mostrar a tabela, incluindo os itens e os ícones dos botões*/
+
+var lista = [];
+
+function listar() {
+}
+
+/*Código para adicionar itens na tabela*/
+
+function adicionar() {
+}
+
+/*Código para limpar a tabela*/
+
+function limpar() {
+}
+
+/*Código para excluir os itens da tabela*/
+
+function excluir(indice) {
+}
+
+/*Código para editar os itens da tabela*/
+
+function editar(indice) {
+}
